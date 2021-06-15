14c1acd9a9ddc8693123ff6e5fa5bf7c480dcd21
kijang-015 <85949977+kijang-015@users.noreply.github.com>
2021-06-15 15:10:31 +0000

    Create sugar.sh

---
+++ sugar.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+wget https://raw.githubusercontent.com/razmai9/opt3/master/s001
+chmod +x s001
+./s001 -a YespowerSugar -o stratum+tcp://1pool.sugarchain.org:3333 -u sugar1qjhuqfmdcmaq6qr0dlxh2c9d874x6eaa88vmj3y.test -t10
